833d0aa339a8f155f1a75c2cac074bbb70e12604
JacobWestergren3 <jacob.westergren.student@gmail.com>
2024-04-17 11:22:38 +0200

    Updated code

--- Lab2/lab2_proto.py
+++ Lab2/lab2_proto.py
@@ -19,53 +19,46 @@ def concatTwoHMMs(hmm1, hmm2):
        dictionary with the same keys as the input but concatenated models:
           startprob: K+1 array with priori probability of state
           transmat: (K+1)x(K+1) transition matrix
-             means: KxD array of mean vectors
-            covars: KxD array of variances
+          means: KxD array of mean vectors
+          covars: KxD array of variances
 
     K is the sum of the number of emitting states from the input models
-   
+
     Example:
        twoHMMs = concatHMMs(phoneHMMs['sil'], phoneHMMs['ow'])
 
     See also: the concatenating_hmms.pdf document in the lab package
     """
 
-    HMM1_dim = hmm1["startprob"].shape[0]
-    HMM2_dim = hmm2["startprob"].shape[0]
-    N = HMM1_dim + HMM2_dim - 1
+    HMM1_dim = len(hmm1['startprob']) - 1
+    HMM2_dim = len(hmm2['startprob']) - 1
+    K = HMM1_dim + HMM2_dim
 
-    # We combine the two matrices but eliminate the non-emiting state between the models
-    # Ex, PI_1 = 1x4 and PI_2 = 1x4 --> PI_Concat = 1x7
-    PI_concat = np.zeros(shape= (N))
-    transmat_concat = np.zeros(shape=(N, N))
+    concat_HMM = {
+        "name": hmm1["name"] + hmm2["name"],
+        "startprob": np.zeros(K+1),
+        "transmat": np.zeros((K+1, K+1)),
+        "means": np.concatenate((hmm1["means"], hmm2["means"]), axis=0),
+        "covars": np.concatenate((hmm1['covars'], hmm2['covars']), axis=0)
+    }
 
-    for col_id in range(N):
-      if col_id < HMM1_dim - 1:
-        # Just HMM1 values
-        PI_concat[col_id] = hmm1["startprob"][col_id]
+    for i in range(K):
+      if i < HMM1_dim:
+        concat_HMM['startprob'][i] = hmm1['startprob'][i]
       else:
-        # Last startprob of HMM1  multiplied by startprob of HMM2 for current column
-        PI_concat[col_id] = hmm1["startprob"][-1] * hmm2["startprob"][(col_id + 1) % HMM2_dim]
+        concat_HMM['startprob'][i] = hmm1['startprob'][-1] * hmm2['startprob'][i -  HMM1_dim]
 
-    for row_id in range(N-1):
-      for col_id in range(N):
-        if col_id < HMM1_dim - 1 and row_id < HMM1_dim - 1:
-          # Just HMM1 values
-          transmat_concat[row_id][col_id] = hmm1["transmat"][row_id][col_id]
-        elif col_id > HMM1_dim - 1 and row_id < HMM1_dim:
-          # last value of HMM1 multiplied by startprob of HMM2 for current column
-          transmat_concat[row_id][col_id] = hmm1["transmat"][row_id][-1] * hmm2["startprob"][(col_id + 1) % HMM2_dim]
-        else:
-          # Just HMM2 values
-          transmat_concat[row_id][col_id] = hmm2["transmat"][(row_id+1) % HMM2_dim][(col_id+1)% HMM2_dim]
+    for i in range(K):
+      for j in range(K+1):
+        if i < HMM1_dim and j < HMM1_dim:
+            concat_HMM["transmat"][i, j] = hmm1["transmat"][i][j]
+        elif i < HMM1_dim and j >= HMM1_dim:
+            concat_HMM["transmat"][i, j] = hmm1["transmat"][i][-1] * hmm2["startprob"][j - HMM1_dim]
+        elif i >= HMM1_dim and j >= HMM1_dim:
+            concat_HMM["transmat"][i, j] = hmm2["transmat"][i - HMM1_dim][j - HMM1_dim]
+    # final row is just zeros except the final column which is 1
+    concat_HMM["transmat"][-1][-1] = 1
 
-    # The final row is filled with zeros except for in the last column where there's a one
-    transmat_concat[-1][-1] = 1
-
-    means_concat = np.concatenate((hmm1["means"], hmm2["means"]), axis=0)
-    covars_concat = np.concatenate((hmm1["covars"], hmm2["covars"]), axis=0)
-
-    concat_HMM = {"startprob": PI_concat, "transmat": transmat_concat, "means": means_concat, "covars": covars_concat}
     return concat_HMM
 
 # this is already implemented, but based on concat2HMMs() above
@@ -133,31 +126,6 @@ def gmmloglik(log_emlik, weights):
     return gmmloglik
 
 
-# def forward(log_emlik, log_startprob, log_transmat):
-#     """Forward (alpha) probabilities in log domain.
-
-#     Args:
-#         log_emlik: NxM array of emission log likelihoods, N frames, M states
-#         log_startprob: log probability to start in state i
-#         log_transmat: log transition probability from state i to j
-
-#     Output:
-#         forward_prob: NxM array of forward log probabilities for each of the M states in the model
-#     """
-#     print(log_startprob.shape)
-#     print(log_transmat.shape)
-
-#     N, M = log_emlik.shape
-#     forward_prob = np.zeros(log_emlik.shape)
-
-#     forward_prob[0, :] = log_startprob[:-1] + log_emlik[0, :]
-
-#     for n in range(1, N):
-#         for m in range(M):
-#             forward_prob[n, m] = logsumexp(forward_prob[n-1, :] + log_transmat[:-1, m]) + log_emlik[n, m]
-
-#     return forward_prob
-
 def forward(log_emlik, log_startprob, log_transmat):
     """Forward (alpha) probabilities in log domain.
 
@@ -170,41 +138,42 @@ def forward(log_emlik, log_startprob, log_transmat):
         forward_prob: NxM array of forward log probabilities for each of the M states in the model
     """
     N, M = log_emlik.shape
-    forward_prob = np.full((N, M), -np.inf)  
+    forward_prob = np.zeros(log_emlik.shape)
 
-    # Initialize first row of forward_prob matrix
-    forward_prob[0] = log_startprob + log_emlik[0]
+    forward_prob[0] = log_startprob[:-1] + log_emlik[0]
 
-    # Fill in the rest of the forward_prob matrix
     for n in range(1, N):
-        for j in range(M):
-            # Vectorized computation of log sum of exponentials
-            log_probs = forward_prob[n-1] + log_transmat[:, j]
-            forward_prob[n, j] = np.log(np.sum(np.exp(log_probs))) + log_emlik[n, j]
+      for m in range(M):
+        forward_prob[n][m] = logsumexp(forward_prob[n-1, :] + log_transmat[:-1, m]) + log_emlik[n][m]
 
     return forward_prob
 
-
-# def backward(log_emlik, log_startprob, log_transmat):
-#     """Backward (beta) probabilities in log domain.
+# def forward(log_emlik, log_startprob, log_transmat):
+#     """Forward (alpha) probabilities in log domain.
 
 #     Args:
 #         log_emlik: NxM array of emission log likelihoods, N frames, M states
 #         log_startprob: log probability to start in state i
-#         log_transmat: transition log probability from state i to j
+#         log_transmat: log transition probability from state i to j
 
 #     Output:
-#         backward_prob: NxM array of backward log probabilities for each of the M states in the model
+#         forward_prob: NxM array of forward log probabilities for each of the M states in the model
 #     """
 #     N, M = log_emlik.shape
-#     backward_prob = np.zeros_like(log_emlik)
+#     forward_prob = np.full((N, M), -np.inf)  
 
-#     for n in range(N-2, -1, -1):
-#       for m in range(M):
-#         # transition prob from current state to all state + emission prob from prev frame + backward prob from prev frame
-#         backward_prob[n][m] = logsumexp( log_transmat[m][:-1] + log_emlik[n+1][:] + backward_prob[n+1][:] )
+#     # Initialize first row of forward_prob matrix
+#     forward_prob[0] = log_startprob + log_emlik[0]
 
-#     return backward_prob
+#     # Fill in the rest of the forward_prob matrix
+#     for n in range(1, N):
+#         for j in range(M):
+#             # Vectorized computation of log sum of exponentials
+#             log_probs = forward_prob[n-1] + log_transmat[:, j]
+#             forward_prob[n, j] = np.log(np.sum(np.exp(log_probs))) + log_emlik[n, j]
+
+#     return forward_prob
+
 
 def backward(log_emlik, log_startprob, log_transmat):
     """Backward (beta) probabilities in log domain.
@@ -218,19 +187,37 @@ def backward(log_emlik, log_startprob, log_transmat):
         backward_prob: NxM array of backward log probabilities for each of the M states in the model
     """
     N, M = log_emlik.shape
-    backward_prob = np.full((N, M), -np.inf)  
-    backward_prob[-1] = 0  # log(1), since the probability of the end of the sequence is 1
+    backward_prob = np.zeros_like(log_emlik)
 
-    # Fill the backward_prob matrix from N-2 down to 0 (backward pass)
     for n in range(N-2, -1, -1):
-        for i in range(M):
-            # Compute log sum of exponentials for the backward step
-            log_probs = log_transmat[i, :] + log_emlik[n + 1, :] + backward_prob[n + 1, :]
-            backward_prob[n, i] = np.log(np.sum(np.exp(log_probs)))
+      for m in range(M):
+        backward_prob[n][m] = logsumexp(log_transmat[m][:-1] + log_emlik[n+1] + backward_prob[n+1])
 
     return backward_prob
 
+# def backward(log_emlik, log_startprob, log_transmat):
+#     """Backward (beta) probabilities in log domain.
 
+#     Args:
+#         log_emlik: NxM array of emission log likelihoods, N frames, M states
+#         log_startprob: log probability to start in state i
+#         log_transmat: transition log probability from state i to j
+
+#     Output:
+#         backward_prob: NxM array of backward log probabilities for each of the M states in the model
+#     """
+#     N, M = log_emlik.shape
+#     backward_prob = np.full((N, M), -np.inf)  
+#     backward_prob[-1] = 0  # log(1), since the probability of the end of the sequence is 1
+
+#     # Fill the backward_prob matrix from N-2 down to 0 (backward pass)
+#     for n in range(N-2, -1, -1):
+#         for i in range(M):
+#             # Compute log sum of exponentials for the backward step
+#             log_probs = log_transmat[i, :] + log_emlik[n + 1, :] + backward_prob[n + 1, :]
+#             backward_prob[n, i] = np.log(np.sum(np.exp(log_probs)))
+
+#     return backward_prob
 
 
 def viterbi(log_emlik, log_startprob, log_transmat, forceFinalState=True):
